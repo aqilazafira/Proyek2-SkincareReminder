7fcc0943415653a3d994291b7a78da8cc8212cd3
Aqila Zafira <hartaticpg@gmail.com>
2024-12-16 22:43:53 +0700

    admin rekomendasi

--- app.py
+++ app.py
@@ -2,9 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("admin.html")
+# Route untuk Admin Dashboard
+@app.route('/admin')
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
 
-if __name__ == "__main__":
+# Route untuk User Dashboard
+@app.route('/user')
+def user_dashboard():
+    return render_template('user_dashboard.html')
+
+if __name__ == '__main__':
     app.run(debug=True)
+
+# Route untuk Admin Rekomendasi
+@app.route('/admin/rekomendasi')
+def admin_rekomendasi():
+    return render_template('admin_rekomendasi.html')
